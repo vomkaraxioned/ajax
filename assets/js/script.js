1e5415687f30c792ed635391aea5f60d53ae79a8
vomkaraxioned <omkarv@axioned.com>
2022-08-30 14:03:08 +0530

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,10 @@
 let key;
 let search = document.querySelector(".city-input span");
 let searchField = document.querySelector(".city-input input");
-const url = "https://api.openweathermap.org/data/2.5/weather?q={delhi}&appid={884256960b2dca1d89e7d4e8e6e894cd}";
+const url = "api.openweathermap.org/data/3.0/onecall?lat=30.489772&lon=-99.771335&units=metric";
 
 search.addEventListener('click', () => {
     key = document.forms['search']['city-name'].value;
     alert(key);
-    fetch(url).then(response => response.json()).then(commits => console.log(commits));
 });
-
-// searchField.addEventListener('keypress', search);
+searchField.addEventListener('keypress', search);
